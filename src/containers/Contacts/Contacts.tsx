613ca3ab9a6c633e9adaf20406cdcd0be166b860
Dilshad Matkulov <dilshadmatkulov@.gmail.com>
2023-11-27 13:08:16 +0600

    added content to Contacts.tsx

--- src/containers/Contacts/Contacts.tsx
+++ src/containers/Contacts/Contacts.tsx
@@ -2,8 +2,15 @@ import React from 'react';
 
 const Contacts: React.FC = () => {
   return (
-    <div>
-      Contacts
+    <div className="text-center">
+      <div className="mb-8">
+        <h1 className="font-bold text-2xl">
+          Contacts
+        </h1>
+        <p className="mt-8 text-blue-200">
+          “ Id urna, nisl, ut quam. Diam suspendisse fringilla quam arcu mattis est velit in. Nibh in purus sit convallis phasellus ut. At vel erat ultricies commodo. Neque suspendisse a habitasse commodo. ”
+        </p>
+      </div>
     </div>
   );
 };
